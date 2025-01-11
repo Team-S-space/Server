d548fd582832e29fe3a724aac7b085d85003d028
ohige01 <ohige01@naver.com>
2025-01-11 19:41:55 +0900

    init project

---
+++ src/main/java/hackathon/spring/service/TempService/TempCommandService.java
@@ -0,0 +1,12 @@
+package hackathon.spring.service.TempService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TempCommandService {
+    public void CheckFlag(Integer flag) {
+
+    }
+}
