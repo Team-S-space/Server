c3652214d6025d24ddb9bf9411071141e82c272e
MODUGGAGI <kiroro0814@naver.com>
2025-01-12 02:18:00 +0900

    feat: ReviewResponseDTO에서 ReviewDetailDTO를 담은 ReviewListDTO 추가

--- src/main/java/hackathon/spring/web/dto/ReviewResponseDTO.java
+++ src/main/java/hackathon/spring/web/dto/ReviewResponseDTO.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class ReviewResponseDTO {
 
@@ -30,5 +31,15 @@ public class ReviewResponseDTO {
 
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
         LocalDateTime created_at;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class ReviewListDTO {
+        List<ReviewResponseDTO.ReviewDetailDTO> reviews;
+        boolean hasNext;
+        Long lastId;
     }
 }
