0231ce2b31382575a8a74182e6881068e18295aa
MODUGGAGI <kiroro0814@naver.com>
2025-01-12 02:15:40 +0900

    feat: ReviewQueryService에 List로 Review 목록 조회

--- src/main/java/hackathon/spring/service/reviewservice/ReviewQueryService.java
+++ src/main/java/hackathon/spring/service/reviewservice/ReviewQueryService.java
@@ -4,4 +4,5 @@ import hackathon.spring.web.dto.ReviewResponseDTO;
 
 public interface ReviewQueryService {
     ReviewResponseDTO.ReviewDetailDTO getReview(Long reviewId);
+    ReviewResponseDTO.ReviewListDTO getReviewList(String region, Integer sunEvent, Long lastId, int limit);
 }
