8950467f0af5d344c6d4aaff67b2c76860bc0a56
ohige01 <ohige01@naver.com>
2025-01-12 06:51:28 +0900

    refactor: home 화면 url 형식 변경

--- src/main/java/hackathon/spring/web/controller/LocationController.java
+++ src/main/java/hackathon/spring/web/controller/LocationController.java
@@ -3,16 +3,12 @@ package hackathon.spring.web.controller;
 import hackathon.spring.apiPayload.ApiResponse;
 import hackathon.spring.apiPayload.code.status.ErrorStatus;
 import hackathon.spring.apiPayload.exception.handler.TempHandler;
-import hackathon.spring.domain.Location;
 import hackathon.spring.domain.enums.Sun;
 import hackathon.spring.service.LocationService.LocationService;
 import hackathon.spring.web.dto.LocationResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,9 +18,9 @@ import java.util.List;
 public class LocationController {
     private final LocationService locationService;
 
-    @GetMapping("/{sunEvent}")
+    @GetMapping("")
     @Operation(summary = "일출, 일몰 명소 조회 API")
-    public ApiResponse<List<LocationResponseDTO.getLocationDTO>> getLocationList(@PathVariable Integer sunEvent) {
+    public ApiResponse<List<LocationResponseDTO.getLocationDTO>> getLocationList(@RequestParam Integer sunEvent) {
         Sun sun;
         if(sunEvent == 0)
             sun = Sun.SUNRISE;
