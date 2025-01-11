6d985d2664673ea8b4b6a3ed18ac8bea37ee4889
ohige01 <ohige01@naver.com>
2025-01-12 06:36:22 +0900

    bug: 중복된 아이디 처리

--- src/main/java/hackathon/spring/repository/TempRepository.java
+++
@@ -1,7 +0,0 @@
-package hackathon.spring.repository;
-
-import hackathon.spring.domain.Temp;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TempRepository extends JpaRepository<Temp, Long> {
-}
